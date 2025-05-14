ac1a2ae6089a6978a9dda25ffbf1ec29a068c47b
Saronzeleke <Cassiopiasaron@gmail.com>
2025-05-14 00:32:39 -0700

    commit

--- index.py
+++ index.py
@@ -57,7 +57,7 @@ class Music(Mood):
         self.command_to_mood = {
             "ayzosh": 1,  
             "selam": 0,
-            "other": 3    # Neutral
+            "other": 3
         }
         # Optional Spotify setup for ETEX (uncomment and configure)
         # import spotipy
