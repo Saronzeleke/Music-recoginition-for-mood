0c89ace955fdccf793865f2074457f0696aed5e2
Saronzeleke <Cassiopiasaron@gmail.com>
2025-05-14 00:39:04 -0700

    commit

--- index.py
+++ index.py
@@ -148,7 +148,8 @@ class Music(Mood):
         return None
 
     def generate_playlist_message(self, place: str = None, context: str = None) -> str:
-       if self.mood
+       if self.mood is None:
+           raise HTTPException(status_code=400,detail="Mood not sent")
         mood_name = {0: "happy", 1: "sad", 2: "angry", 3: "relaxed"}[self.mood]
         if context == "period_relaxation" and place == "taxi":
             return f"Ayzosh, relax in the taxi with these soothing Protestant {mood_name} hymns."
